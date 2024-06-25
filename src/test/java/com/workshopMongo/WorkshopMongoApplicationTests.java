5f8a2cc992586ede0600049d3e925d01cffeae16
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-06-25 09:25:01 -0300

    Projeto criado

---
+++ src/test/java/com/workshopMongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
